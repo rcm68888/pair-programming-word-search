5441d814189b7a3d3132a80c46737e5d89d9f5ab
Rcm <rcm68888@gmail.com>
2023-12-23 16:06:16 -0700

    1stcommit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
+// pair programming between Mike & Rey
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  let result = false;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let vertical = [];
+  for (let row = 0; row < letters.length; row++) {
+    for (let column = 0; column < letters[row].length; column++) {
+        if (row === 0) {
+          vertical.push([letters[row][column]]);
+        } else {
+          vertical[column].push(letters[row][column]);
+      }
     }
+  }        
+  
+  const verticalJoin = vertical.map(ls => ls.join(''));
+
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      result = true;
+    }
+  }
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return result;
 }
 
 module.exports = wordSearch
